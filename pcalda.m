3956c4306a19c2d9c4eccd08bcdb0df63e581468
Overxfl0w <overxfl0w13@hotmail.com>
2015-04-25 01:06:24 +0200

    Update pcalda.m

--- pcalda.m
+++ pcalda.m
@@ -1,20 +1,20 @@
 #!/usr/bin/octave -qf
-if(nargin!=8)
-    printf("Usage: pcalda.m <type> <trdata> <trlabels> <tsdata> <tslabels> <mink> <stepk> <maxk>\n");
+if(nargin!=9)
+    printf("Usage: pcalda.m <type> <trdata> <trlabels> <tsdata> <tslabels> <mink> <stepk> <maxk> <result_file> \n");
     exit(1);
 end
 
 # Argumentos #
 arg_list=argv();
-type   = arg_list{1};
-trdata = arg_list{2};
-trlabs = arg_list{3};
-tsdata = arg_list{4};
-tslabs = arg_list{5};
-mink   = str2num(arg_list{6});
-stepk  = str2num(arg_list{7});
-maxk   = str2num(arg_list{8});
-
+type     = arg_list{1};
+trdata   = arg_list{2};
+trlabs   = arg_list{3};
+tsdata   = arg_list{4};
+tslabs   = arg_list{5};
+mink     = str2num(arg_list{6});
+stepk    = str2num(arg_list{7});
+maxk     = str2num(arg_list{8});
+nameFile = arg_list{9};
 # Carga de archivos #
 load(trdata);
 load(trlabs);
@@ -30,7 +30,7 @@ if(type=="ORI")
 	printf("Error en el espacio original : %f\n",knn(X,xl,Y,yl,1));
 end
 
-fd = fopen("results.txt","w");
+fd = fopen(nameFile,"w");
 ## PCA ##
 if(type=="PCA")
 	fwrite(fd,"# This file has PCA results #\r\n");
@@ -65,9 +65,9 @@ if(type=="LDA")
 	fclose(fd);
 end
 
-## PCA y LDA generando la gráfica ##
+## PCA y LDA separados generando la gráfica ##
 if(type=="PLA")
-	fwrite(fd,"# This file has PCA and LDA results #\r\n");
+	fwrite(fd,"# This file has PCA and LDA results separated#\r\n");
 	fwrite(fd,"# Dimensions\t%ErrorPCA\t%ErrorLDA\r\n");
 	while mink<=maxk
 		[m,WPca]  = pca(X,mink);
@@ -88,6 +88,63 @@ if(type=="PLA")
 	system("gnuplot PCALDA.gp");
 end	
 
+## PCA Y LDA combinados (No son necesarios los 4 ultimos parametros), configurar a mano en la competicion ##
+## Primero se aplica PCA proyectando hasta nº dimensiones de la imagen, después se aplica LDA sobre las 5 dimensiones que con PCA 
+## den menor error ##
+if(type=="PLC")
+	minPCA     = 1;   # Ajustar dimension minima de PCA AQUI #
+	maxPCA     = 256; # Ajustar dimension maxima de PCA AQUI #
+	menoresErrorPCA = [];
+	menoresDimPCA   = [];
+	kk         = 1; # Configurar numero de vecinos del clasificador #
+	while minPCA<maxPCA
+		[avg,W] = pca(X,minPCA);
+		proyX   = W'*X;
+		proyY   = W'*Y;
+		err     = knn(proyX,xl,proyY,yl,kk);
+		if(minPCA<=5) # Coger las 5 dimensiones que den menor error con PCA #
+		    menoresErrorPCA(end+1) = err;
+		    menoresDimPCA(end+1) = minPCA;
+		else
+		    if(err<max(menoresErrorPCA))
+		        i = find(menoresErrorPCA==max(menoresErrorPCA));
+		        menoresErrorPCA(i) = err;
+		        menoresDimPCA(i) = minPCA;
+		    end
+		end
+		minPCA = minPCA + 1;
+		#disp("Con error: "),disp(menoresErrorPCA),disp(" dimension: "),disp(menoresDimPCA)
+	endwhile
+	disp("Menores con PCA ya calculados: "),disp(menoresDimPCA),disp(menoresErrorPCA)
+	# Calcular con los 5 menores de PCA el error con LDA y coger el menor #
+	x = 1;
+	j = 1;
+	#menoresDimPCA = [10,20,30,40,50];
+	menorError = 100;
+	dimPCA = 0;
+	dimLDA = 0;
+	for x=1:5 # Con las 5 dimensiones de PCA que dan menor error #
+	    for j=1:9 # Probar con nº de clases dimensiones de LDA (Ajustar dimensiones LDA AQUI)#
+	        [avg,WPCA]  = pca(X,menoresDimPCA(x));
+	        proyXPCA = WPCA'*X;
+	        proyYPCA = WPCA'*Y;
+	        WLDA     = lda(proyXPCA,xl,j);
+	        proyXLDA = WLDA'*proyXPCA;
+	        proyYLDA = WLDA'*proyYPCA;
+	        err      = knn(proyXLDA,xl,proyYLDA,yl,kk);
+	        #disp("Calculado"),disp(x),disp(j),disp("con error: "),disp(err);
+	        if(err<menorError)
+	            dimPCA = x;
+	            dimLDA = j;
+	            menorError = err;
+	            #disp("Menor error con: "),disp(dimPCA),disp(dimLDA),disp(" : "),disp(err)
+	        end   
+	     endfor
+	endfor
+	disp("Menor error con: "),disp(dimPCA),disp(dimLDA),disp(" : "),disp(menorError)
+	fclose(fd); 
+end	
+	
 ## PCA y Original generando la gráfica ##
 if(type=="PCO")
 	fwrite(fd,"# This file has PCA and Original results #\r\n");
